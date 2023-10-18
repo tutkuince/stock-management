4c0a384e61f000a8b39a21eca7a140fb5a170f94
Tutku Ince <tutku.ince@outlook.com>
2023-10-18 13:29:25 +0300

    Product Entity has been created

---
+++ product-service/src/main/java/io/ince/stockmanagement/productservice/repository/entity/Product.java
@@ -0,0 +1,42 @@
+package io.ince.stockmanagement.productservice.repository.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "product", schema = "stock_management")
+public class Product {
+    @Id
+    @Column(name = "product_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long productId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "price")
+    private double price;
+
+    @Builder.Default
+    @Column(name = "product_updated_date")
+    private LocalDateTime productUpdatedDate = LocalDateTime.now();
+
+    @Builder.Default
+    @Column(name = "product_created_date")
+    private LocalDateTime productCreatedDate = LocalDateTime.now();
+
+    @Column(name = "is_active")
+    private boolean isActive;
+}
