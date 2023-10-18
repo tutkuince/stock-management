1b9bfe38caf9863b25440b84d230d3d8f202e06e
Tutku Ince <tutku.ince@outlook.com>
2023-10-18 14:06:27 +0300

    InternalAPIResponse has been created for Response

---
+++ product-service/src/main/java/io/ince/stockmanagement/productservice/response/InternalAPIResponse.java
@@ -0,0 +1,17 @@
+package io.ince.stockmanagement.productservice.response;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Data
+@Builder
+public class InternalAPIResponse<T> {
+    private FriendlyMessage friendlyMessage;
+    private T payload;
+    private boolean hasError;
+    private List<String> errorMessages;
+    private HttpStatus httpStatus;
+}
