9e92c9876e25e197bda111276f5083885bc25b5a
Tutku Ince <tutku.ince@outlook.com>
2023-10-18 13:43:07 +0300

    Create And Update Product classes have been created

---
+++ product-service/src/main/java/io/ince/stockmanagement/productservice/request/ProductUpdatedRequest.java
@@ -0,0 +1,11 @@
+package io.ince.stockmanagement.productservice.request;
+
+import lombok.Data;
+
+@Data
+public class ProductUpdatedRequest {
+    private Long productId;
+    private String productName;
+    private Integer quantity;
+    private Double price;
+}
