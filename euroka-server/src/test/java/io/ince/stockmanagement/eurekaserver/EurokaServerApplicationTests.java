0bf83966e18c8d8adbe9a841446a02d549fce7a5
Tutku Ince <tutku.ince@outlook.com>
2023-10-19 17:35:02 +0300

    Eureka Server Project has been created and configured

---
+++ euroka-server/src/test/java/io/ince/stockmanagement/eurekaserver/EurokaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ince.stockmanagement.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurokaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
